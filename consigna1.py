3994123a58ba5a09053f0971ad60386d4b482668
Stefano <stefanopavesa@gmail.com>
2023-11-05 18:18:13 -0300

    commit

---
+++ consigna1.py
@@ -0,0 +1,12 @@
+numero = int(input("Ingrese su número: "))
+primo = True
+
+for i in range(2, numero):
+    if numero % i == 0:
+        primo = False
+        break
+
+if primo:
+    print("Es primo")
+else:
+    print("No es primo")
