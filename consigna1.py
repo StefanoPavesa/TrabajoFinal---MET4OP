b5fd12af034e374d0990432b086c0ef2d6340f38
milagros <mili.llagonne@gmail.com>
2023-11-06 21:00:02 -0300

    Commit

--- consigna1.py
+++ consigna1.py
@@ -10,5 +10,15 @@ if primo:
     print("Es primo")
 else:
     print("No es primo")
-   
+    
+
+numero = float((input("Ingrese un numero para saber si es primo: ")))
+nro = int(numero)
+numero_primo = (nro/nro == 1) or (nro/nro == nro)
+if nro == numero_primo:     
+    print(f"{numero} es un número primo")
+else:
+    print(f"{numero} no es un número primo")
+    
+
 
